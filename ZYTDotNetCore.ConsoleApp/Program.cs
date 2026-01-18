6c7cf30288d61b4652eea4b0e787eab17f5f81a1
Zayar397 <zayartun0880@gmail.com>
2026-01-18 11:38:04 +0630

    Add project files.

---
+++ ZYTDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ZYTDotNetCore.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}

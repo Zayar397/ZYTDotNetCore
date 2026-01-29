4f1a7ce6951c5124d4d1dd2016a6a15f4b5173ac
Zayar397 <zayartun0880@gmail.com>
2026-01-29 22:38:20 +0630

    feat: Other class for EFCore

--- ZYTDotNetCore.ConsoleApp/Program.cs
+++ ZYTDotNetCore.ConsoleApp/Program.cs
@@ -9,12 +9,16 @@ namespace ZYTDotNetCore.ConsoleApp
         {
             //ADO_DOT_NET adoDotNet = new ADO_DOT_NET();
             //adoDotNet.Delete();
-            DAPPER dapper = new DAPPER();
+            //DAPPER dapper = new DAPPER();
             //dapper.CREATE("B012520","ThetP","Testing...");
             //dapper.Edit(1);
             //dapper.Edit(100);
             //dapper.UPDATE(7,"B012610","xxx","Update Testing...");
-            dapper.DELETE(7);
+            //dapper.DELETE(7);
+            EFCORE efCore = new EFCORE();
+            //efCore.READ();
+            //efCore.CREATE("A012910","Yoon Waddy","Testing...");
+            efCore.EDIT(1006);
         }
     }
 }

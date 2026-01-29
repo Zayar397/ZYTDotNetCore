052bedd76a6e195978ff4d7d350d374bfb5191a7
Zayar397 <zayartun0880@gmail.com>
2026-01-29 22:37:33 +0630

    feat: Add configuration for EFCore

---
+++ ZYTDotNetCore.ConsoleApp/DotNetTrainingBatch5DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZYTDotNetCore.ConsoleApp.Models;
+
+namespace ZYTDotNetCore.ConsoleApp
+{
+    public class DotNetTrainingBatch5DbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source = DESKTOP-BPF6HTF\\SQL2022; Initial Catalog = DotNetTrainingBatch5; User Id = sa; Password = p@ssw0rd; Trust Server Certificate = True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}

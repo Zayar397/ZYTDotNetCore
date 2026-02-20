b894209d91de930c16bd83b4dd408b3286738c50
Zayar397 <zayartun0880@gmail.com>
2026-02-20 22:09:28 +0630

    feat: Add Post, Put and Delete using ADO.NET

--- ZYTDotNetCore.RestApi/Controllers/BlogsController_AdoDotNet.cs
+++ ZYTDotNetCore.RestApi/Controllers/BlogsController_AdoDotNet.cs
@@ -41,25 +41,100 @@ namespace ZYTDotNetCore.RestApi.Controllers
             return Ok(blogList);
             conn.Close();
         }
-        //[HttpGet("{blogId}")]
-        //public IActionResult GetBlogs(int blogId)
-        //{
-
-        //}
-        //[HttpPost]
-        //public IActionResult CreateBlogs(TblBlog blog)
-        //{
-
-        //}
-        //[HttpPut("{blogId}")]
-        //public IActionResult UpdateBlogs(int blogId, TblBlog blog)
-        //{
-
-        //}
+        [HttpGet("{blogId}")]
+        public IActionResult GetBlogs(int blogId)
+        {
+            List<BlogViewModel> blogList = new List<BlogViewModel>();
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            string selectQuery = @"Select * from TBL_BLOG
+                                    where BlogId = @BlogId";
+            SqlCommand cmd = new SqlCommand(selectQuery, conn);
+            cmd.Parameters.AddWithValue("@BlogId", blogId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                blogList.Add(new BlogViewModel
+                {
+                    Id = Convert.ToInt32(reader["BlogId"]),
+                    Title = Convert.ToString(reader["BlogTitle"]),
+                    Author = Convert.ToString(reader["BlogAuthor"]),
+                    Content = Convert.ToString(reader["BlogContent"]),
+                    DeleteFlage = Convert.ToBoolean(reader["DeleteFlage"])
+                });
+            }
+            return Ok(blogList);
+            conn.Close();
+        }
+        [HttpPost]
+        public IActionResult CreateBlogs(BlogViewModel blogViewModel)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            string insertQuery = @"INSERT INTO [dbo].[TBL_BLOG]
+                                           ([BlogTitle]
+                                           ,[BlogAuthor]
+                                           ,[BlogContent]
+                                           ,[DeleteFlage])
+                                     VALUES
+                                           (@BlogTitle
+                                           ,@BlogAuthor
+                                           ,@BlogContent
+                                           ,0)";
+            SqlCommand cmd = new SqlCommand(insertQuery,conn);
+            if (!string.IsNullOrEmpty(blogViewModel.Title))
+            {
+                cmd.Parameters.AddWithValue("@BlogTitle", blogViewModel.Title);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@BlogTitle", "");
+            }
+            if (!string.IsNullOrEmpty(blogViewModel.Author))
+            {
+                cmd.Parameters.AddWithValue("@BlogAuthor", blogViewModel.Author);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@BlogAuthor", "");
+            }
+            if (!string.IsNullOrEmpty(blogViewModel.Content))
+            {
+                cmd.Parameters.AddWithValue("@BlogContent", blogViewModel.Content);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@BlogContent", "");
+            }
+            int recordCount = cmd.ExecuteNonQuery();
+            conn.Close();
+            return Ok(recordCount == 1 ? "Record inserted successfully." : "Record inserted failed.");
+        }
+        [HttpPut("{blogId}")]
+        public IActionResult UpdateBlogs(int blogId, BlogViewModel blogViewModel)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            string updateQuery = @"UPDATE [dbo].[TBL_BLOG]
+                                       SET [BlogTitle] = @BlogTitle
+                                          ,[BlogAuthor] = @BlogAuthor
+                                          ,[BlogContent] = @BlogContent
+                                          ,[DeleteFlage] = @DeleteFlage
+                                     WHERE BlogId = @BlogId";
+            SqlCommand cmd = new SqlCommand(updateQuery,conn);
+            cmd.Parameters.AddWithValue("@BlogTitle",blogViewModel.Title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blogViewModel.Author);
+            cmd.Parameters.AddWithValue("@BlogContent", blogViewModel.Content);
+            cmd.Parameters.AddWithValue("@DeleteFlage", blogViewModel.DeleteFlage);
+            cmd.Parameters.AddWithValue("@BlogId",blogId);
+            int recordCount = cmd.ExecuteNonQuery();
+            conn.Close();
+            return Ok(recordCount == 1 ? "Record updated successfully." : "Record updated failed.");
+        }
         [HttpPatch("{blogId}")]
         public IActionResult PatchBlogs(int blogId, BlogViewModel blogViewModel)
         {
-            string condition="";
+            string condition = "";
             if (!string.IsNullOrEmpty(blogViewModel.Title))
             {
                 condition += " [BlogTitle] = @BlogTitle, ";
@@ -76,7 +151,7 @@ namespace ZYTDotNetCore.RestApi.Controllers
             {
                 return BadRequest("Invalid parameters.");
             }
-            condition = condition.Substring(0,condition.Length-2);
+            condition = condition.Substring(0, condition.Length - 2);
 
             SqlConnection conn = new SqlConnection(_connectionString);
             conn.Open();
@@ -101,10 +176,19 @@ namespace ZYTDotNetCore.RestApi.Controllers
             conn.Close();
             return Ok(recordCount > 0 ? "Record updated successfully." : "Record updated fail");
         }
-        //[HttpDelete("{blogId}")]
-        //public IActionResult DeleteBlogs(int blogId)
-        //{
-
-        //}
+        [HttpDelete("{blogId}")]
+        public IActionResult DeleteBlogs(int blogId)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            string deleteQuery = @"UPDATE [dbo].[TBL_BLOG]
+                                       SET [DeleteFlage] = 1
+                                     WHERE BlogId = @BlogId";
+            SqlCommand cmd = new SqlCommand(deleteQuery,conn);
+            cmd.Parameters.AddWithValue("@BlogId", blogId);
+            int recordCount = cmd.ExecuteNonQuery();
+            conn.Close();
+            return Ok(recordCount == 1 ? "Record deleted successfully." : "Record deleted failed.");
+        }
     }
 }

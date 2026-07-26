996bd37d04fc09dba9e55cbeb1e96b9636f616e2
Zayar397 <zayartun0880@gmail.com>
2026-07-26 16:55:05 +0630

    feat: Add apex chare

---
+++ ZYTDotNetCore.ChartWebApp/Controllers/ApexChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZYTDotNetCore.ChartWebApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ ZYTDotNetCore.ChartWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZYTDotNetCore.ChartWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
